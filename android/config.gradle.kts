e4f399d6eeb7924578a7fa59709678bbca9fb0d5
Cengiz <cengiz@puzly.co>
2025-07-26 21:57:13 +0600

    Excluded .uid and .import files from archive.

--- android/config.gradle.kts
+++ android/config.gradle.kts
@@ -12,7 +12,7 @@ extra.apply {
 
 	// Godot
 	set("godotVersion", "4.5")
-	set("releaseType", "beta3")
+	set("releaseType", "stable")
 	set("godotAarUrl", "https://github.com/godotengine/godot-builds/releases/download/${get("godotVersion")}-${get("releaseType")}/godot-lib.${get("godotVersion")}.${get("releaseType")}.template_release.aar")
 	set("godotAarFile", "godot-lib-${get("godotVersion")}.${get("releaseType")}.aar")
 
