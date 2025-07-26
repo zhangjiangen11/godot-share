d8e15ad65f91c27c63de5c59cc0b49f1d62a6d44
Cengiz <cengiz@puzly.co>
2025-07-26 16:09:48 +0600

    Updated demo app to show correct image & cleanup.

--- demo/Main.gd
+++ demo/Main.gd
@@ -10,12 +10,24 @@ extends Node
 @onready var share: Share = $Share as Share
 @onready var _text_edit: TextEdit = $CanvasLayer/CenterContainer/VBoxContainer/VBoxContainer/TextEdit as TextEdit
 @onready var _label: RichTextLabel = $CanvasLayer/CenterContainer/VBoxContainer/RichTextLabel as RichTextLabel
-@onready var _texture_rect: TextureRect = $CanvasLayer/CenterContainer/VBoxContainer/TextureRect as TextureRect
+@onready var _android_texture_rect: TextureRect = $CanvasLayer/CenterContainer/VBoxContainer/HBoxContainer/AndroidTextureRect as TextureRect
+@onready var _ios_texture_rect: TextureRect = $CanvasLayer/CenterContainer/VBoxContainer/HBoxContainer/iOSTextureRect as TextureRect
+
+var _active_texture_rect: TextureRect
+
+
+func _ready() -> void:
+	if OS.has_feature("ios"):
+		_android_texture_rect.hide()
+		_active_texture_rect = _ios_texture_rect
+	else:
+		_ios_texture_rect.hide()
+		_active_texture_rect = _android_texture_rect
 
 
 func _on_share_texture_button_pressed() -> void:
 	_print_to_screen("Sharing texture")
-	share.share_texture(_texture_rect.texture, shared_title, shared_subject, _text_edit.text)
+	share.share_texture(_active_texture_rect.texture, shared_title, shared_subject, _text_edit.text)
 
 
 func _on_share_viewport_button_pressed() -> void:
